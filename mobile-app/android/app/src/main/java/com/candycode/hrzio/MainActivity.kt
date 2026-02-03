5244a4fd7970f5ffa4ce547604de16b580424bb6
CandyCode Tech <director@candycodetech.com>
2026-02-03 22:38:58 +0530

    Configure Firebase for push notifications
    - Add google-services.json for Android Firebase configuration
    - Add Google Services plugin to build.gradle files
    - Update package name to com.candycode.hrzio to match Firebase config
    - Move Kotlin files to new package directory
    - Add Firebase admin SDK files to .gitignore
    
    Co-Authored-By: CandyCode Technologies

--- mobile-app/android/app/src/main/java/com/candycode/hrzio/MainActivity.kt
+++ mobile-app/android/app/src/main/java/com/candycode/hrzio/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hrmobile
+package com.candycode.hrzio
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- mobile-app/android/app/src/main/java/com/candycode/hrzio/MainApplication.kt
+++ mobile-app/android/app/src/main/java/com/candycode/hrzio/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.hrmobile
+package com.candycode.hrzio
 
 import android.app.Application
 import com.facebook.react.PackageList
